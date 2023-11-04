d09d589ef8025e89a53c759190869b6509db6501
Fuseji04 <fiw11za0802@gmail.com>
2023-11-04 18:56:36 +0700

    test

--- client/src/components/HelloWorld.vue
+++ client/src/components/HelloWorld.vue
@@ -1,45 +1,61 @@
 <template>
   <div class="hello">
-    <center>
-      <p>แนะนำโดยคุณฟิว</p>
-      <button v-on:click="navigateTo('/waters')">ดูข้อมูลผู้ใช้</button>
-    </center>
+    <div class="background-image">
+      <div class="container-fluid">
+        <p>แนะนำโดยคุณฟิว</p>
+        <button v-on:click="navigateTo('/waters')">ดูข้อมูลผู้ใช้</button>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  data () {
+  data() {
     return {
       msg: 'Welcome to Your Vue.js App'
-    } 
+    };
   },
   methods: {
-        navigateTo(route) {
-            this.$router.push(route)
-        }
-    },
-
-  
-
-  
-}
+    navigateTo(route) {
+      this.$router.push(route);
+    }
+  }
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.background-image {
+  text-align: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+  height: 100vh;
+}
+
+.container-fluid {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  height: 100%;
+}
+
 h1, h2 {
   font-weight: normal;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
   color: #42b983;
 }
